38bade619ea852a243da1887caabae8f79331df1
Professional <tnorum@yandex.ru>
2021-03-11 15:05:41 +0300

    Сделан калькулятор обыкновенных дробей

--- calculator_practice/CalculatorObDrob.cpp
+++ calculator_practice/CalculatorObDrob.cpp
@@ -1,7 +1,7 @@
 #include "CalculatorObDrob.h"
-#include <iostream>
 
 using namespace std;
+
 
 void Menu_calculator_ob_drob()
 {
@@ -22,116 +22,274 @@ void Menu_calculator_ob_drob()
 		unsigned short a;
 
 		cin >> a;
+		system("cls");
 		switch (a)
 		{
 		case 1:
 			Addition_of_fractions();
+			system("pause");
 			break;
 
 		case 2:
 			Subtraction_of_fractions();
+			system("pause");
+
 			break;
 
 		case 3:
 			Multiplication_of_fractions();
+			system("pause");
+
 			break;
 
 		case 4:
 			Dividing_fractions();
+			system("pause");
+
 			break;
 
 		case 5:
 			Comparing_fractions();
+			system("pause");
+
 			break;
 
 		case 6:
 			Reduction_of_fractions();
+			system("pause");
+
 			break;
 
 		case 7:
-			Decimal_representation();
+			to_Decimal();
+			system("pause");
+
 			break;
 		case 0:
 			quite = true;
 			break;
 
 		}
+		
 	}
 
 }
+
 
 void Addition_of_fractions()
 {
-	int plusChislitel1 = 0, plusChislitel2 = 0, plusZnamenatel1 = 0, plusZnamenatel2 = 0;
-	int chislitel, znamenatel; // - переменные, которые уже получаютс€ при сложении 
+	fraction f1, f2, result;
 
-	do {
-		cout << "¬ведите числитель первой дроби из интервала [-100;100] : " << endl;
-		cin >> plusChislitel1;
-	} while (plusChislitel1 > 100 || plusChislitel1 < -100);
-	do {
-		cout << "¬ведите знаменатель дл€ первой дроби из интервала [-100;100] исключа€ 0: " << endl;
-		cin >> plusZnamenatel1;
-	} while (plusZnamenatel1 == 0);
-	do {
-		cout << "¬ведите числитель второй дроби из интервала [-100;100]: " << endl;
-		cin >> plusChislitel2;
-	} while (plusChislitel2 > 100 || plusChislitel2 < -100);
-	do {
-		cout << "¬ведите знаменатель второй дроби из интервала [-100;100] исключа€ 0: " << endl;
-		cin >> plusZnamenatel2;
-	} while (plusZnamenatel2 == 0);
+	enter_fraction(f1, f2);
 
-	znamenatel = plusZnamenatel1 * plusZnamenatel2;
-	chislitel = plusChislitel1 * plusZnamenatel2 + plusChislitel2 * plusZnamenatel1;
+	result.z = NOK(f1.z, f2.z);
+	result.c = f1.c * result.z / f1.z + f2.c * result.z / f2.z;
 
-	if (chislitel < znamenatel)
-	{
-		cout << plusChislitel1 << " " << plusChislitel2 << " " << chislitel << endl;
-		cout << "Ч" << " + " << "Ч" << " = " << "ЧЧ" << endl;
-		cout << plusZnamenatel1 << " " << plusZnamenatel2 << " " << znamenatel << endl;
-	}
-	else
-	{
-		if (chislitel == znamenatel)
-		{
-			cout << plusChislitel1 << " " << plusChislitel2 << " " << endl;
-			cout << "Ч" << " + " << "Ч" << "= 1" << endl;
-			cout << plusZnamenatel1 << " " << plusZnamenatel2 << " " << endl;
-		}
-		else
-		{
-			int integer = 0;
-			integer = chislitel / znamenatel;
-
-			cout << plusChislitel1 << " " << plusChislitel2 << " " << chislitel - integer * znamenatel << endl;
-			cout << "Ч" << " + " << "Ч" << " = " << integer << "ЧЧ" << endl;
-			cout << plusZnamenatel1 << " " << plusZnamenatel2 << " " << znamenatel << endl;
-		}
-
-	}
+	print_fraction(f1, f2, result, '+');
 }
+
 
 void Subtraction_of_fractions()
 {
+	fraction f1, f2, result;
+
+	enter_fraction(f1, f2);
+
+
+	result.z = NOK(f1.z, f2.z);
+	result.c = f1.c * result.z / f1.z - f2.c * result.z / f2.z;
+
+	print_fraction(f1, f2, result, '-');
 }
+
 
 void Multiplication_of_fractions()
 {
+	fraction f1, f2, result;
+
+	enter_fraction(f1, f2);
+
+	result.c = f1.c * f2.c;
+	result.z = f1.z * f2.z;
+
+	print_fraction(f1, f2, result, '*');
 }
+
 
 void Dividing_fractions()
 {
+	fraction f1, f2, result;
+
+	enter_fraction(f1, f2);
+
+	result.c = f1.c * f2.z;
+	result.z = f1.z * f2.c;
+
+	print_fraction(f1, f2, result, ':');
 }
+
 
 void Comparing_fractions()
 {
+	fraction f1, f2;
+	int temp1, temp2;
+	enter_fraction(f1, f2);
+
+	int nok = NOK(f1.z, f2.z);
+
+	temp1 = f1.c * nok / f1.z;
+	temp2 = f2.c* nok / f2.z;
+
+	char c = temp1 > temp2 ? '>':'<';
+	print_fraction(f1, f2, c);
 }
+
 
 void Reduction_of_fractions()
 {
+	fraction f1;
+	enter_fraction(f1);
+
+	double a = (double)f1.c / f1.z;
+
+	print_fraction(f1, a);
 }
 
-void Decimal_representation()
+
+void to_Decimal()
 {
+	
+}
+
+
+void print_fraction(fraction f1,fraction f2, fraction result, char sign)
+{
+	int ac1 = amount_chars(f1);
+	int ac2 = amount_chars(f2);
+	int ac3 = amount_chars(result);
+
+	cout << setw(ac1) << f1.c << "   " << setw(ac2) << f2.c << "   " << setw(ac3) << result.c <<endl;
+
+	for (int i = 0; i < ac1; i++)
+		cout << '-';
+	cout << " " << sign << " ";
+	for (int i = 0; i < ac2; i++)
+		cout << '-';
+	cout << " = ";
+	for (int i = 0; i < ac3; i++)
+		cout << '-';
+	cout << endl;
+
+	cout << setw(ac1) << f1.z << "   " << setw(ac2) << f2.z << "   " << setw(ac3) << result.z << endl;
+
+
+}
+
+
+void print_fraction(fraction f1,  fraction f2, char sign)
+{
+	int ac1 = amount_chars(f1);
+	int ac2 = amount_chars(f2);
+
+	cout << setw(ac1) << f1.c << "   " << setw(ac2) << f2.c << endl;
+
+	for (int i = 0; i < ac1; i++)
+		cout << '-';
+	cout << " " << sign << " ";
+	for (int i = 0; i < ac2; i++)
+		cout << '-';
+
+
+	cout << endl;
+
+	cout << setw(ac1) << f1.z << "   " << setw(ac2) << f2.z << endl;
+}
+
+
+void print_fraction(fraction f1, double result)
+{
+	int ac1 = amount_chars(f1);
+
+	cout << setw(ac1) << f1.c << "   " << endl;
+
+	for (int i = 0; i < ac1; i++)
+		cout << '-';
+
+	cout <<   " = " << result<<endl;
+
+	cout << setw(ac1) << f1.z << endl;
+}
+
+
+void enter_fraction(fraction &f1)
+{
+	cout << "¬ведите числитель : ";
+	cin >> f1.c;
+	cout << "¬ведите знаменатель : ";
+	cin >> f1.z;
+	while (f1.z == 0)
+	{
+		cout << "«наменатель не может быть равен 0!" << endl;
+		cout << "¬ведите знаменатель : ";
+		cin >> f1.z;
+	}
+}
+
+
+void enter_fraction(fraction& f1, fraction& f2)
+{
+	cout << "ѕерва€ дробь."<<endl;
+	enter_fraction(f1);
+	cout << "¬тора€ дробь." << endl;
+	enter_fraction(f2);
+
+}
+
+
+int NOK(int denominator1, int denominator2)
+{
+	int i = denominator1 < denominator2 ? denominator1 : denominator2;
+	for (; i <= denominator1 * denominator2; i++)
+	{
+		if (i % denominator1 == 0 && i % denominator2 == 0)
+			return i;
+	}
+}
+
+
+int NOD(fraction f1)
+{
+	int nod = 1;
+
+	for (int i = 2; i <= f1.c && i <= f1.z; i++)
+	{
+		if (f1.c % i == 0 && f1.z % i == 0)
+			nod = i;
+	}
+
+	return nod;
+}
+
+
+int amount_chars(fraction& f)
+{
+	int count_1, count_2, t;
+
+	count_1 = count_2 = 0;
+	t = f.c;
+
+	while (t > 0)
+	{
+		t /= 10;
+		count_1++;
+	}
+
+	t = f.z;
+	while (t > 0)
+	{
+		t /= 10;
+		count_2++;
+	}
+	
+	return count_1 > count_2 ? count_1 : count_2;
 }
--- calculator_practice/CalculatorObDrob.h
+++ calculator_practice/CalculatorObDrob.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
 
 void Menu_calculator_ob_drob();
 
@@ -8,4 +10,22 @@ void Multiplication_of_fractions();
 void Dividing_fractions();
 void Comparing_fractions();
 void Reduction_of_fractions();
-void Decimal_representation();
+void to_Decimal();
+
+struct fraction
+{
+	int c = 0;
+	int z = 0;
+
+};
+
+int amount_chars(fraction& f);
+int NOK(int denominator1, int denominator2);
+int NOD(fraction f1);
+
+void print_fraction(fraction f1, double result);
+void print_fraction(fraction f1, fraction f2, char sign);
+void print_fraction(fraction f1, fraction f2, fraction result, char sign);
+
+void enter_fraction(fraction& f1);
+void enter_fraction(fraction& f1, fraction& f2);
