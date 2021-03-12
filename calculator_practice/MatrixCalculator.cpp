4b77ab083d9b1b5e1ca9cb0c1b9e66bf4bd8aeda
YungYoku <63597891+YungYoku@users.noreply.github.com>
2021-03-12 20:33:18 +0300

    Матрицы

--- calculator_practice/MatrixCalculator.cpp
+++ calculator_practice/MatrixCalculator.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 struct Matrix {
 	double array[MAX_ROW][MAX_COL]{};
-	int columnsAmount = 0, rowsAmount = 0;
+	int columnsAmount = 0, rowsAmount = 0, size = 0;
 };
 
 void Menu_matrix_calculator()
@@ -65,38 +65,26 @@ void Menu_matrix_calculator()
 	}
 }
 
-void matrixEnter(Matrix& matrix, bool isMatrixSquare = false) {
-	if (isMatrixSquare) {
-		do {
-			cout << "Введите размер матрицы: ";
-			cin >> matrix.columnsAmount;
-			if (matrix.columnsAmount < 1 || matrix.columnsAmount> MAX_COL) {
-				cout << "Размер  может быть только от 1 до 20" << endl;
-			}
-		} while (matrix.columnsAmount < 1 || matrix.columnsAmount> MAX_COL);
-	}
-	else {
-		do {
-			cout << "Введите количество столбцов матрицы: " << endl;
-			cin >> matrix.columnsAmount;
-			if (matrix.columnsAmount < 1 || matrix.columnsAmount> MAX_COL) {
-				cout << "Размер  может быть только от 1 до 20" << endl;
-			}
-		} while (matrix.columnsAmount < 1 || matrix.columnsAmount> MAX_COL);
+void matrixEnter(Matrix& matrix) {
+	do {
+		cout << "Введите количество столбцов матрицы: " << endl;
+		cin >> matrix.columnsAmount;
+		if (matrix.columnsAmount < 1 || matrix.columnsAmount> MAX_COL) {
+			cout << "Размер  может быть только от 1 до 20" << endl;
+		}
+	} while (matrix.columnsAmount < 1 || matrix.columnsAmount> MAX_COL);
 
-		do {
-			cout << "Введите количество строк матрицы: "<< endl;
-			cin >> matrix.rowsAmount;
-			if (matrix.rowsAmount < 1 || matrix.rowsAmount> MAX_ROW) {
-				cout << "Размер  может быть только от 1 до 20" << endl;
-			}
-		} while (matrix.rowsAmount < 1 || matrix.rowsAmount> MAX_ROW);
-	}
+	do {
+		cout << "Введите количество строк матрицы: "<< endl;
+		cin >> matrix.rowsAmount;
+		if (matrix.rowsAmount < 1 || matrix.rowsAmount> MAX_ROW) {
+			cout << "Размер  может быть только от 1 до 20" << endl;
+		}
+	} while (matrix.rowsAmount < 1 || matrix.rowsAmount> MAX_ROW);
 }
 
 void matrixPrint(Matrix matrix) {
-
-	cout << "\nИтоговая матрица";
+	cout << "\nИтоговая матрица\n";
 
 	for (int i = 0; i < matrix.rowsAmount; i++) {
 		cout << endl;
@@ -106,7 +94,6 @@ void matrixPrint(Matrix matrix) {
 	}
 
 	cout << endl;
-
 }
 
 void addition_matrix()
@@ -136,12 +123,12 @@ void subtraction_matrix()
 
 	matrixEnter(matrix);
 
-	for (int s = 0; s < 2; s++) {
-		cout << "Введите матрицу " << s + 1 << ":" << endl;
+	for (int n = 0; n < 2; n++) {
+		cout << "Введите матрицу " << n + 1 << ":" << endl;
 		for (int i = 0; i < matrix.rowsAmount; i++) {
 			for (int j = 0; j < matrix.columnsAmount; j++) {
 				cin >> entryTempo;
-				if (s == 0) {
+				if (n == 0) {
 					matrix.array[i][j] += entryTempo;
 				}
 				else {
@@ -159,17 +146,15 @@ void multiplication_matrix()
 	Matrix matrix[3];
 
 	do {
-		cout << "\nМатрица 1:\n";
-		matrixEnter(matrix[0]);
-
-		cout << "\nМатрица 2:\n";
-		matrixEnter(matrix[1]);
+		for (int n = 0; n < 2; n++) {
+			cout << "\nМатрица " << n+1 <<":\n";
+			matrixEnter(matrix[n]);
+		}
 
 		if (matrix[0].columnsAmount != matrix[1].rowsAmount) {
 			cout << "\nКоличество столбцов в первой матрице, должно совпадать с количеством строк во второй матрице\n";
 		}
 	} while (matrix[0].columnsAmount != matrix[1].rowsAmount);
-
 
 	for (int s = 0; s < 2; s++) {
 		cout << "Введите матрицу " << s + 1 << ":" << endl;
@@ -187,8 +172,6 @@ void multiplication_matrix()
 			}
 		}
 	}
-
-	
 
 	matrix[2].columnsAmount = matrix[0].columnsAmount;
 	matrix[2].rowsAmount = matrix[1].rowsAmount;
@@ -239,136 +222,138 @@ void matrix_transpose()
 void inverse_matrix()
 {
 	Matrix matrix;
-
-	matrixEnter(matrix, true);
+	do {
+		cout << "\nВведите размер матрицы: ";
+		cin >> matrix.size;
+		cout << endl;
+		if (matrix.size < 1) {
+			cout << "\nРазмер матрицы не может быть меньше 1\n";
+		}
+	} while (matrix.size < 1);
 
 	cout << "\nВведите матрицу\n";
-	for (int i = 0; i < matrix.columnsAmount; i++) {
-		for (int j = 0; j < matrix.columnsAmount; j++) {
+	for (int i = 0; i < matrix.size; i++) {
+		for (int j = 0; j < matrix.size; j++) {
 			cin >> matrix.array[i][j];
 		}
 	}
 
-	/*for (int i = 0; i < matrixSize; i++) {
-		for (int j = 0; j < 2 * matrixSize; j++) {
-			// Добавление 1 в диагональных местах
-			// матрицы для создания идентичности
+	double temp;
+	double newMatrix[MAX_ROW][MAX_COL];
 
-			if (j == (i + matrixSize)) {
-				matrix[i][j] = 1;
+	for (int i = 0; i < matrix.size; i++) {
+		for (int j = 0; j < matrix.size; j++)
+		{
+			newMatrix[i][j] = 0;
+			if (i == j) {
+				newMatrix[i][j] = 1;
+			}				
+		}
+	}
+
+	for (int k = 0; k < matrix.size; k++) {
+		temp = matrix.array[k][k];
+
+		for (int j = 0; j < matrix.size; j++)
+		{
+			matrix.array[k][j] /= temp;
+			newMatrix[k][j] /= temp;
+		}
+
+		for (int i = k + 1; i < matrix.size; i++)
+		{
+			temp = matrix.array[i][k];
+
+			for (int j = 0; j < matrix.size; j++)
+			{
+				matrix.array[i][j] -= matrix.array[k][j] * temp;
+				newMatrix[i][j] -= newMatrix[k][j] * temp;
 			}
 		}
 	}
 
-	// Меняем строку матрицы,
-	// смена ряда начнется с последнего ряда
+	for (int k = matrix.size - 1; k > 0; k--) {
+		for (int i = k - 1; i >= 0; i--)
+		{
+			temp = matrix.array[i][k];
 
-	float temp;
-
-	for (int i = matrixSize - 1; i > 0; i--) {
-		// Меняем местами каждый элемент двух строк
-		if (matrix [i - 1] [0] <matrix [i] [0])
-		for (int j = 0; j < 2 * matrixSize; j ++) {
-			// Перестановка строки, если выше
-			// условие выполнено.
-
-			temp = matrix [i] [j];
-			matrix [i] [j] = matrix [i - 1] [j];
-			matrix [i - 1] [j] = temp;
-		}
-
-		// Прямой обмен строк с помощью указателей экономит время
-
-		if (matrix[i - 1][0] < matrix[i][0]) {
-			int temp = matrix[i][0];
-			matrix[i][0] = matrix[i - 1][0];
-			matrix[i - 1][0] = temp;
-		}
-	}
-
-	// Замена строки на сумму самой себя и
-	// константы, кратной другой строке матрицы
-
-	for (int i = 0; i < matrixSize; i++) {
-		for (int j = 0; j < matrixSize; j++) {
-			if (j != i) {
-				temp = matrix[j][i] / matrix[i][i];
-
-				for (int k = 0; k < 2 * matrixSize; k++) {
-					matrix[j][k] -= matrix[i][k] * temp;
-				}
+			for (int j = 0; j < matrix.size; j++)
+			{
+				matrix.array[i][j] -= matrix.array[k][j] * temp;
+				newMatrix[i][j] -= newMatrix[k][j] * temp;
 			}
 		}
 	}
 
-	// Умножаем каждую строку на ненулевое целое число.
-	// Делим элемент строки по диагональному элементу
-
-	for (int i = 0; i < matrixSize; i++) {
-		temp = matrix[i][i];
-
-		for (int j = 0; j < 2 * matrixSize; j++) {
-			matrix[i][j] = matrix[i][j] / temp;
-		}
-	}
-
-	//Выводим результирующую обратную матрицу.
-
-	cout << "\nОБРАТНАЯ МАТРИЦА\n";
-	for (int i = 0; i < matrixSize; i++) {
-		for (int j = matrixSize; j < 2 * matrixSize; j++) {
-			cout << matrix[i][j] << " ";
+	cout << "\nОбратная матрица\n";
+	for (int i = 0; i < matrix.size; i++)
+	{
+		for (int j = 0; j < matrix.size; j++) {
+			cout << newMatrix[i][j] << "  ";
 		}
 		cout << endl;
-	}*/
+	}
+}
 
-	cout << endl;
+// Determinant
+
+void matrixWithoutRowAndCol(double matrix[MAX_ROW][MAX_COL], int size, int column, double newMatrix[MAX_ROW][MAX_COL]) {
+	int offsetCol = 0;
+	for (int i = 0; i < size - 1; i++) {
+		offsetCol = 0;
+		for (int j = 0; j < size - 1; j++) {
+			if (j == column) {
+				offsetCol = 1;
+			}
+			newMatrix[i][j] = matrix[i + 1][j + offsetCol];
+		}
+	}
+}
+
+double matrixDet(double matrix[MAX_ROW][MAX_COL], int size) {
+	double det = 0;
+	int degree = 1;
+
+	if (size == 1) {
+		return matrix[0][0];
+	}
+	else if (size == 2) {
+		return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+	}
+	else {
+		double newMatrix[MAX_ROW][MAX_COL];
+		for (int j = 0; j < size; j++) {
+			matrixWithoutRowAndCol(matrix, size, j, newMatrix);
+
+			det += degree * matrix[0][j] * matrixDet(newMatrix, size - 1);
+
+			degree = -degree;
+		}
+
+	}
+
+	return det;
 }
 
 void determinant()
 {
 	Matrix matrix;
-	int matrixSize;
+	do {
+		cout << "\nВведите размер матрицы: ";
+		cin >> matrix.size;
+		cout << endl;
+		if (matrix.size < 1) {
+			cout << "\nРазмер матрицы не может быть меньше 1\n";
+		}
+	} while (matrix.size < 1);
 
-	matrixEnter(matrix, true);
-
+	
 	cout << "\nВведите матрицу\n";
-	for (int i = 0; i < matrix.columnsAmount; i++) {
-		for (int j = 0; j < matrix.columnsAmount; j++) {
+	for (int i = 0; i < matrix.size; i++) {
+		for (int j = 0; j < matrix.size; j++) {
 			cin >> matrix.array[i][j];
 		}
 	}
 
-	int p = 0, determinant = 0;
-	for (int i = 0; i < matrix.columnsAmount - 1; i++)
-	{
-		int t = 1;
-		while (matrix.array[i][i] == 0)
-		{
-			for (int j = 0; j < matrix.columnsAmount; j++)
-			{
-				matrix.array[i][j] = determinant;
-				matrix.array[i][j] = matrix.array[i + t][j];
-				matrix.array[i + t][j] = determinant;
-			}
-			p++;
-			t++;
-		}
-
-		for (int k = i + 1; k < matrix.columnsAmount; k++)
-		{
-			determinant = matrix.array[k][i] / matrix.array[i][i];
-			for (int j = 0; j < matrix.columnsAmount; j++)
-				matrix.array[k][j] -= matrix.array[i][j] * determinant;
-		}
-	}
-
-	determinant = pow(-1, p);
-
-	for (int i = 0; i < matrix.columnsAmount; i++) {
-		determinant *= matrix.array[i][i];
-	}
-
-	cout << "Определитель равен: " << determinant << endl;
-
+	cout << "Определитель равен: " << matrixDet(matrix.array, matrix.size) << endl;
 }
